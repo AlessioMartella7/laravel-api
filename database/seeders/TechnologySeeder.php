c297955f4bd10b51373fd1a665f1d759b7cd861c
AlessioMartella7 <alessio.mart87@gmail.com>
2024-11-07 15:34:11 +0100

    seeded data into technologies table

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,42 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $programmingLanguages = [
+            "Python",
+            "Java",
+            "C#",
+            "Ruby",
+            "PHP",
+            "Node.js",
+            "Go",
+            "Rust",
+            "Swift",
+            "Kotlin",
+            "JavaScript",
+            "TypeScript",
+            "HTML",
+            "CSS",
+            "Sass",
+            "React",
+            "Vue.js",
+            "Angular",
+            "Elm",
+            "Dart",
+            "SQL",
+            "MongoDB",
+            "GraphQL",
+            "Redis",
+            "SQLite",
+            "PostgreSQL",
+            "MySQL",
+            "MariaDB"
+        ];
+
+        foreach ($programmingLanguages as $singleProgrammingLanguage) {
+            $technology = new Technology();
+            $technology->name = $singleProgrammingLanguage;
+
+            $technology->save();
+        }
     }
 }
