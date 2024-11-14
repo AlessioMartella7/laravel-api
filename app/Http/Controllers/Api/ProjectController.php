0ca4fe13446a6a2543b7e2e7e95f55a06e86c701
AlessioMartella7 <alessio.mart87@gmail.com>
2024-11-14 15:39:55 +0100

    added index method in Api\ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -12,7 +13,11 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::with('type','technologies')->get();
+            return response()->json([
+                'success' => true,
+                'response' => $projects
+            ]);
     }
 
     /**
