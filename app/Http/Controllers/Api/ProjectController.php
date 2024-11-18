b221c5c711deb8bab4300fdace20d5930207a31e
AlessioMartella7 <alessio.mart87@gmail.com>
2024-11-18 18:34:09 +0100

    fixed api\show method

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -41,6 +41,7 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
+        $project = Project::with('type','technologies')->findOrFail($project->id);
         return response()->json([
             'success' => true,
             'response' => $project
